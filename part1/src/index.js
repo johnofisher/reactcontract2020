9242d3175e64ef99f26a8b429e9d543be6dcad18
johnofisher <46865855+johnofisher@users.noreply.github.com>
2019-10-23 11:02:14 +0100

    ..

---
+++ part1/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Content =  {
+  return (
+
+  )
+}
+
+const App = () => {
+  // const-definitions
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content  />
+      <Total  />
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
